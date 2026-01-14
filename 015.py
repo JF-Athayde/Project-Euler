8be6d42e930ea59086a78aed92a2efdd0460e934
João Fellipe Coutinho Athayde <athaydefellipe@gmail.com>
2026-01-14 01:30:44 -0300

    Problem number 15
    JF
    This is problem number 15 solve.
    From: Brasil

---
+++ 015.py
@@ -0,0 +1,13 @@
+x = 20
+
+dp = [[0] * (x+1) for _ in range(x+1)]
+
+for i in range(x+1):
+    dp[0][i] = 1
+    dp[i][0] = 1
+
+for i in range(1, x+1):
+    for j in range(1, x+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+print(dp[-1][-1])
